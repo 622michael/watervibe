f398bab193d9e5bad42f55dae2e7cd450bf7a840
Michael Crabtree <michael@dyn157068.cc.lehigh.edu>
2016-10-03 15:44:45 -0400

    added subscription verification

--- fitbit/urls.py
+++ fitbit/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from . import authorization, views
+from . import authorization, views, subscription
 
 urlpatterns = [
 	url(r'^authorize', authorization.authorize, name='authorize'),
 	url(r'^button', views.permission_button, name='button'),
 	url(r'^subscribe', authorization.permissions_request, name='permissions'),
+	url(r'^notication/sleep', )
 ]
