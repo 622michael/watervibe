69e250bd55b94ec4ff17ea19754304bd53c277a9
Michael Crabtree <michael@dyn158203.cc.lehigh.edu>
2016-10-23 23:17:15 -0400

    render location changed

--- fitbit/views.py
+++ fitbit/views.py
@@ -7,14 +7,14 @@ import authorization
 # Create your views here.
 
 def authorization_success(scope, request):
-	return render(request, 'fitbit/success.html', {})
+	return render(request, 'success.html', {})
 
 def authorization_failed(errors, request):
-	return render(request, 'fitbit/failed.html', {})
+	return render(request, 'failed.html', {})
 
 
 def permission_button(request):
-	return render(request,'fitbit/button.html', {})
+	return render(request,'button.html', {})
 
 
 def request_permissions(request): 
@@ -25,4 +25,4 @@ def alarms_full(request):
 	return error(request, "You have too many alarms on your FitBit (max is 8). Please remove at least one and register again.")
 
 def error(request, message):
-	return render(request, 'fitbit/error.html', {'message' : message})
+	return render(request, 'error.html', {'message' : message})
