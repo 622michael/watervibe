a8104930538972b79dfec3e2ccf39a0e4d7a944e
Michael Crabtree <michael@dyn158203.cc.lehigh.edu>
2016-10-25 11:25:50 -0400

    validating database

--- watervibe/events/events.py
+++ watervibe/events/events.py
@@ -3,6 +3,7 @@
 ## require some calculations like duration.
 
 import importlib
+from watervibe.models import Event
 
 ## Returns the given event duration
 ## in minutes.
@@ -55,3 +56,15 @@ def fringe_time_for_event(tag):
 def adjust_reminder_at(reminder_time, event):
 	event_abstract = importlib.import_module("watervibe.events." + event.tag)
 	return event_abstract.adjust_reminder_at(reminder_time, event)
+
+## This insures that there is an active
+## sleep event on every day of the week.
+## If there is not one is set for 8:30pm - 8:30am
+def validate_user_sleeps(user):
+	for day in range(0,8):
+			sleep_events = Event.objects.filter(user = user, day_of_week = day, is_active = 1, tag = "sleep")
+			if len(sleep_events) == 0: 
+				default_sleep_event = Event.objects.filter(user = user, day_of_week = day, tag = "sleep", start_time = 8.5, end_time = 8.5).first()
+
+				if default_sleep_event is None: 
+					default_sleep_event = Event.objects.create(user = user, day_of_week = day, tag = "sleep", start_time = 8.5, end_time = 8.5, is_active = 1)
--- watervibe/management/commands/validate_database.py
+++ watervibe/management/commands/validate_database.py
@@ -1,22 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from watervibe.models import User, Event
+from watervibe.events import events
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-
 		for user in User.objects.all():
-			validate_user_sleeps(user)
-			
-			
-
-	## This insures that there is an active
-	## sleep event on every day of the week.
-	## If there is not one is set for 8:30pm - 8:30am
-	def validate_user_sleeps(user):
-		for day in range(0,8):
-				sleep_events = Event.objects.filter(user = user, day_of_week = day, is_active = 1, tag = "sleep")
-				if len(sleep_events) == 0: 
-					default_sleep_event = Event.objects.filter(user = user, day_of_week = day, tag = "sleep", start_time = 8.5, end_time = 8.5).first()
-
-					if default_sleep_event is None: 
-						default_sleep_event = Event.objects.create(user = user, day_of_week = day, tag = "sleep", start_time = 8.5, end_time = 8.5, is_active = 1)
+			events.validate_user_sleeps(user)
